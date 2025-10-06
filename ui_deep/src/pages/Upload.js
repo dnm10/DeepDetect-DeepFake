7529cca2e92cafd1833c0901537ce93787f00e7f
dnm10 <deeptinm10@gmail.com>
2025-10-06 14:49:08 +0530

    Localhost

--- ui_deep/src/pages/Upload.js
+++ ui_deep/src/pages/Upload.js
@@ -33,7 +33,7 @@ function Upload() {
     setResult(null);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict/", {
+      const response = await fetch("http://localhost:8000/predict/", {
         method: "POST",
         body: formData,
       });
